8b6fdb9ed429f883ee4ce5fa0a65cae14a19d484
Scott <clearplaid@gmail.com>
2019-06-25 23:21:57 -0400

    commented out CDC redirect bc it popped up immediately, commented out original load index page route need to integrate with mine, added my seeds test, added cocktails.hbs and cocktails.js for future use and to connect current main page functionality, added main page display, modal code, and route to cocktails/:id

---
+++ models/seedsTest.sql
@@ -0,0 +1,17 @@
+INSERT INTO Cocktails (name, category, glass, pic, instructions, ing, createdAt, updatedAt)
+VALUES ("Mojito" , "Cocktail" , "Highball glass" , "https://www.thecocktaildb.com/images/media/drink/rxtqps1478251029.jpg" , "Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish and serve with straw.","Light rum, 2 -Lime, Juice of 1 -Sugar, 2 tsp -Mint, 3 -Soda water ", now(), now());
+
+INSERT INTO Cocktails (name, category, glass, pic, instructions, ing, createdAt, updatedAt)
+VALUES ("Old Fashioned" , "Cocktail" , "Old-fashioned glass" , "https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg" , "Place sugar cube in old fashioned glass and saturate with bitters, add a dash of plain water. Muddle until dissolved.\r\nFill the glass with ice cubes and add whiskey.\r\n\r\nGarnish with orange twist, and a Cocktail_tab cherry." , "Bourbon, 4.5 cL -Angostura bitters, 2 dashes -Sugar, 1 cube -Water, dash ", now(), now());
+
+INSERT INTO Cocktails (name, category, glass, pic, instructions, ing, createdAt, updatedAt)
+VALUES ("Long Island Tea" , "Ordinary Drink" , "Highball glass" , "https://www.thecocktaildb.com/images/media/drink/ywxwqs1439906072.jpg" , "Combine all ingredients (except cola) and pour over ice in a highball glass. Add the splash of cola for color. Decorate with a slice of lemon and serve." , "Vodka, 1/2 oz -Light rum, 1/2 oz -Gin, 1/2 oz -Tequila, 1/2 oz -Lemon water, Juice of 1/2 -Coca-Cola, 1 splash ", now(), now());
+
+INSERT INTO Cocktails (name, category, glass, pic, instructions, ing, createdAt, updatedAt)
+VALUES ("Negroni" , "Ordinary Drink" , "Old-fashioned glass" , "https://www.thecocktaildb.com/images/media/drink/tutwwv1439907127.jpg" , "Stir into glass over ice, garnish and serve." , "Gin, 1 oz /Campari, 1 oz /Sweet Vermouth, 1 oz ", now(), now());
+
+INSERT INTO Cocktails (name, category, glass, pic, instructions, ing, createdAt, updatedAt)
+VALUES ("Whiskey Sour" , "Ordinary Drink" , "Old-fashioned glass" , "https://www.thecocktaildb.com/images/media/drink/o56h041504352725.jpg" , "Shake with ice. Strain into chilled glass, garnish and serve. If served 'On the rocks', strain ingredients into old-fashioned glass filled with ice." ,"Blended whiskey, 2 oz -Lemon, Juice of 1/2 -Powdered sugar, 1/2 tsp -Cherry, 1 -Lemon , 1/2 slice ", now(), now());
+
+INSERT INTO Cocktails (name, category, glass, pic, instructions, ing, createdAt, updatedAt)
+VALUES ("Dry Martini" , "Cocktail" , "Cocktail glass" , "https://www.thecocktaildb.com/images/media/drink/71t8581504353095.jpg", "Straight: Pour all ingredients into mixing glass with ice cubes. Stir well. Strain in chilled martini cocktail glass. Squeeze oil from lemon peel onto the drink, or garnish with olive.", "Gin, 2/3 oz -Dry Vermouth, 1/3 oz -Olive, 1 ", now(), now());
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,19 +29,19 @@ module.exports = function(app) {
   });
 };*/
 
-  app.get("/", function(req,res){ // "age" is the input from age page.
-    let age = req.body.age;       // format is MMDDYYYY, for exp: 07101990
+  // app.get("/", function(req,res){ // "age" is the input from age page.
+  //   let age = req.body.age;       // format is MMDDYYYY, for exp: 07101990
                             
-    let dif = moment( age , "MMDDYYY").fromNow();
-    if (parseInt(dif)>=21){
-      res.redirect('/index');
-      console.log("User passed 21");
-    }
-    else {
-      res.redirect('https://www.cdc.gov/alcohol/fact-sheets/minimum-legal-drinking-age.htm');
-      console.log("User is under 21");
-    }
-  });
+  //   let dif = moment( age , "MMDDYYY").fromNow();
+  //   if (parseInt(dif)>=21){
+  //     res.redirect('/index');
+  //     console.log("User passed 21");
+  //   }
+  //   else {
+  //     res.redirect('https://www.cdc.gov/alcohol/fact-sheets/minimum-legal-drinking-age.htm');
+  //     console.log("User is under 21");
+  //   }
+  // });
 
   app.get("/api/api_cocktail", function(req, res) { // bottom api show button
     db.Custom_drink.findAll({}).then(function(db) {
@@ -104,5 +104,33 @@ module.exports = function(app) {
         .then(data => res.json(data))
     }
   })
+
+  // Get all cocktails
+  app.get("/api/cocktails", function(req, res) {
+    db.Cocktails.findAll({}).then(function(dbCocktails) {
+      res.json(dbCocktails);
+    });
+  });
+
+  // Written for future use for custom drinks
+  // Create a new cocktail
+  app.post("/api/cocktails", function(req, res) {
+    db.Cocktails.create(req.body).then(function(dbCocktails) {
+      res.json(dbCocktails);
+    });
+  });
+
+  // Delete a cocktail by id
+  app.delete("/api/cocktails/:id", function(req, res) {
+    db.Cocktails.destroy({ where: { id: req.params.id } }).then(function(
+      dbCocktails
+    ) {
+      res.json(dbCocktails);
+    });
+  });
+
+
+
+// end of module.exports
 };
 
