796532c72cca281bcab7e2ff296a7366e415c8f2
unknown <baris.sahin.can@gmail.com>
2019-06-25 21:14:13 -0400

    Age asking Added, All created coctail show data routes Added, custom_drink table added(sequelize)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,56 @@
 var db = require("../models");
 var passport = require("../config/passport");
+var moment = require('moment');
 
 module.exports = function(app) {
+
+  /*// Get all examples
+  app.get("/api/examples", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.json(dbExamples);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/examples", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.json(dbExample);
+    });
+  });
+};*/
+
+  app.get("/", function(req,res){ // "age" is the input from age page.
+    let age = req.body.age;       // format is MMDDYYYY, for exp: 07101990
+                            
+    let dif = moment( age , "MMDDYYY").fromNow();
+    if (parseInt(dif)>=21){
+      res.redirect('/index');
+      console.log("User passed 21");
+    }
+    else {
+      res.redirect('https://www.cdc.gov/alcohol/fact-sheets/minimum-legal-drinking-age.htm');
+      console.log("User is under 21");
+    }
+  });
+
+  app.get("/api/api_cocktail", function(req, res) { // bottom api show button
+    db.Custom_drink.findAll({}).then(function(db) {
+      res.json(db);
+    });
+  });
+
   // user login
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.redirect('/')
+    res.redirect('/index')
 
     // if (req.user) {
     //   res.send("logged in");
@@ -32,28 +78,5 @@ module.exports = function(app) {
     req.logout();
     res.redirect("/");
     console.log("logout successful");
-  });
-
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
   });
 };
